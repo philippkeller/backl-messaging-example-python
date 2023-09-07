f6fc18d7fabe2557b6313be3cf1aa8816081fa5d
Philipp Keller <philipp.keller@homegate.ch>
2023-09-07 07:09:57 +0200

    huh!

--- api/index.py
+++ api/index.py
@@ -4,8 +4,6 @@ app = Flask(__name__)
 
 @app.route('/message', methods=['POST'])
 def message():
-    import time
-    time.sleep(10)
     p = request.get_json()
     subject = 'Links on your site "{prospect_title}"'.format(**p)
     email = p["email_address"]
